c5b7b6e5716112c0e1784e20bbf1f8328a64f7fa
edwinpjdcesar <edwinpjdcesar@gmail.com>
2015-05-12 23:51:22 -0500

    Update min.css
    More

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,29 @@
-// JavaScript source code
+/*
+
+
+    Edwin Patrick Cesar
+    edwinpjdcesar@gmail.com
+    edwincesar.com
+    main.js
+
+
+*/
+
+$(document).ready(function () {
+
+    // scrollspy and smooth scroll
+    $('a[href*=#][class*="smooth"]:not([href=#])').click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+        return false;
+    });
+
+    $('body').scrollspy({ target: '#theNav' });
+
+    $('[data-spy="scroll"]').each(function () {
+        var $spy = $(this).scrollspy('refresh')
+    })
+
+});
+
