1a2c5e4034239f4ce3e78b06015e4d9acaddd99d
Edwin Cesar <edwinpjdcesar@gmail.com>
2015-05-19 15:43:13 -0400

    Navbar Glitch
    Fix it then

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,17 +12,19 @@
 $(document).ready(function () {
 
     // scrollspy and smooth scroll
-    $('a[href*=#][class*="smooth"]:not([href=#])').click(function () {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 500);
-        return false;
-    });
+    
 
     $('body').scrollspy({ target: '#theNav' });
 
     $('[data-spy="scroll"]').each(function () {
         var $spy = $(this).scrollspy('refresh')
+    });
+
+    $('a[href*=#][class*="smooth"]:not([href=#])').click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+        return false;
     });
 
 });
