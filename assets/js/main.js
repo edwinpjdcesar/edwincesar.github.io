41cef772bb923780acf433800452f761f270774f
edwinpjdcesar <edwinpjdcesar@gmail.com>
2015-05-21 03:46:57 -0500

    Begin JQuery Shirt Designer Demo
    Just need to provide options

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,19 +11,38 @@
 
 $(document).ready(function () {
 
-    // scrollspy and smooth scroll
+    //  scrollspy
     $('body').scrollspy({ target: '#theNav' });
 
     $('[data-spy="scroll"]').each(function () {
         var $spy = $(this).scrollspy('refresh')
     });
-
+    //  smooth scroll
     $('a[href*=#][class*="smooth"]:not([href=#])').click(function () {
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top
         }, 500);
         return false;
     });
+
+    //      jquery demo
+
+    //  image draggable within canvas
+    $(function () {
+        $(".drag").draggable({
+            containment: $('.canvas')
+        });
+    });
+    //  resize image
+    $(".smaller").click(function () {
+        var it = $(".image");
+        it.css({ 'width': '-=20', 'height': '-=20' });
+    });
+    $(".bigger").click(function () {
+        var it = $(".image");
+        it.css({ 'width': '+=20', 'height': '+=20' });
+    });
+   
 
 });
 
